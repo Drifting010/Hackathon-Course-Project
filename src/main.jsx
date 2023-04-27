1ee2305f271a00ccfc230e8d9ca2efc1e554b8ec
liuyj <yanjie.liu@hotmail.com>
2023-04-28 10:44:18 +1200

    empty Vite cli

--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { createServer } from 'miragejs'
-import axios from 'axios'
-import App from './App'
-import makeServer from './server'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'https://api.realworld.io/api'
-}
-
-const defaultQueryFn = async ({ queryKey }) => {
-  const { data } = await axios.get(queryKey[0], { params: queryKey[1] })
-  return data
-}
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryFn: defaultQueryFn,
-      staleTime: 300000,
-    },
-  },
-})
-
-if (window.Cypress && process.env.NODE_ENV === 'test') {
-  const cyServer = createServer({
-    routes() {
-      ;['get', 'put', 'patch', 'post', 'delete'].forEach((method) => {
-        this[method]('/*', (schema, request) => window.handleFromCypress(request))
-      })
-    },
-  })
-  cyServer.logging = false
-} else if(process.env.NODE_ENV === 'development') {
-  makeServer({ environment: 'development' })
-}
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} containerElement="div" />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
