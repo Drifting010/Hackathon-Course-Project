71da2ef6c01da2c215094be18d5ef0f5f36d576b
Drason Guo <dguo430@aucklanduni.ac.nz>
2023-05-02 05:26:49 +1200

    Added some css and extra details

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import { Button, InputAdornment, Paper, TextField } from "@mui/material";
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import styles from './Login.module.css'
+import GoogleIcon from '@mui/icons-material/Google';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Login() {
     return (
@@ -8,13 +13,37 @@ function Login() {
             <h1>Log in to your account</h1>
             <p>Welcome back</p>
             <div>
-                <Button>Google</Button>
-                <Button>GitHub</Button>
-                <Button>Linkedin</Button>
-                <Button>Facebook</Button>
+                <Button
+                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
+                    variant="contained"
+                    startIcon={<GoogleIcon/>}
+                >
+                    Google
+                </Button>
+                <Button
+                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
+                    variant="contained"
+                    startIcon={<GitHubIcon/>}
+                >
+                    GitHub
+                </Button>
+                <Button
+                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
+                    variant="contained"
+                    startIcon={<LinkedInIcon/>}
+                >
+                    Linkedin
+                </Button>
+                <Button
+                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
+                    variant="contained"
+                    startIcon={<FacebookIcon/>}
+                >
+                    Facebook
+                </Button>
             </div>
             <p>or continue with email</p>
-            <div>
+            <div className={styles.container}>
                 <TextField
                     required
                     id="email-required"
@@ -42,6 +71,15 @@ function Login() {
                         ),
                         }}
                 />
+            </div>
+            <a href="">Forgot password?</a>
+            <div>
+                <Button
+                    sx={{backgroundColor: 'orange', ':hover': {backgroundColor: 'sandybrown'},width: '250px'}}
+                    variant="contained"
+                >
+                    Log in
+                </Button>
             </div>
         </div>
     )
