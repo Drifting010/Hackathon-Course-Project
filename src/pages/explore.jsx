15aea1fe2cc696b47478056704a05a9b851e3ec9
xli340 <xli340@aucklanduni.ac.nz>
2023-05-02 20:34:28 +1200

    Solve the coding style problem that import in jsx uses single quotes and other parts use double quotes

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -25,57 +25,57 @@ export default function Explopre() {
       <CssBaseline />
       <Box
         sx={{
-          bgcolor: 'background.paper',
+          bgcolor: "background.paper",
           pt: 8,
           pb: 2,
         }}
       >
         {/* three button */}
-        <Container maxWidth='md'>
+        <Container maxWidth="md">
           <Stack
             sx={{ pt: 4 }}
-            direction='row'
+            direction="row"
             spacing={2}
-            justifyContent='flex-start'
+            justifyContent="flex-start"
           >
-            <Button variant='outlined'>All</Button>
-            <Button variant='outlined'>Ongoing</Button>
-            <Button variant='outlined'>Finished</Button>
+            <Button variant="outlined">All</Button>
+            <Button variant="outlined">Ongoing</Button>
+            <Button variant="outlined">Finished</Button>
           </Stack>
         </Container>
       </Box>
       {/* display cards */}
-      <Container sx={{ py: 2 }} maxWidth='md'>
+      <Container sx={{ py: 2 }} maxWidth="md">
         <Grid container spacing={4}>
           {cards.map((card) => (
             <Grid item key={card} xs={12} sm={6} md={4}>
               <Card
                 sx={{
-                  height: '100%',
-                  display: 'flex',
-                  flexDirection: 'column',
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
                 }}
-                data-testid='card'
+                data-testid="card"
               >
                 <CardMedia
-                  component='img'
+                  component="img"
                   sx={{}}
-                  image='https://source.unsplash.com/random'
-                  alt='random'
+                  image="https://source.unsplash.com/random"
+                  alt="random"
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant='h5' component='h2'>
+                  <Typography gutterBottom variant="h5" component="h2">
                     title
                   </Typography>
                   <Typography>prize pool $1000</Typography>
-                  <Stack direction='row' alignItems='center' spacing={2}>
+                  <Stack direction="row" alignItems="center" spacing={2}>
                     <LinearProgress
-                      sx={{ height: 10, width: '60%' }}
-                      color='secondary'
-                      variant='determinate'
+                      sx={{ height: 10, width: "60%" }}
+                      color="secondary"
+                      variant="determinate"
                       value={50}
                     />
-                    <Typography fontSize='10px'>Apply in 30 days</Typography>
+                    <Typography fontSize="10px">Apply in 30 days</Typography>
                   </Stack>
                   <Typography>ongoing</Typography>
                 </CardContent>
