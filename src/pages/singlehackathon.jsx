d701ed3f850d97dce8b7da3e1e46eeeb8cb2a042
liuyj <yanjie.liu@hotmail.com>
2023-05-03 07:43:28 +1200

    basic structure and layout COMPLETE

--- src/pages/singlehackathon.jsx
+++ src/pages/singlehackathon.jsx
@@ -5,7 +5,7 @@ export default function Singlehackathon() {
         prize_pool: 30000,
         start_date: '2023/05/02',
         end_date: '2023/05/10',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat dui eget quam vestibulum, in mollis elit fermentum. Aenean tristique lectus condimentum mollis aliquet. Maecenas vehicula tincidunt aliquet. Donec mattis ipsum sit amet varius tincidunt. Maecenas varius eros neque, et bibendum tellus euismod nec. Nulla justo neque, vestibulum eu dolor ut, venenatis congue libero. Curabitur ullamcorper, risus eu lacinia lobortis, dolor libero rutrum lorem, id cursus elit nisl sit amet velit. Aliquam ac pharetra felis. Sed in velit sit amet orci volutpat mollis. Fusce euismod tincidunt tortor, ut varius nibh. Sed urna metus, finibus nec sodales eu, congue vitae sapien. Vivamus imperdiet molestie est quis blandit',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat dui eget quam vestibulum, in mollis elit fermentum. Aenean tristique lectus condimentum mollis aliquet. Maecenas vehicula tincidunt aliquet. Donec mattis ipsum sit amet varius tincidunt. Maecenas varius eros neque, et bibendum tellus euismod nec. Nulla justo neque, vestibulum eu dolor ut, venenatis congue libero. Curabitur ullamcorper, risus eu lacinia lobortis, dolor libero rutrum lorem, id cursus elit nisl sit amet velit. Aliquam ac pharetra felis. Sed in velit sit amet orci volutpat mollis. Fusce euismod tincidunt tortor, ut varius nibh. Sed urna metus, finibus nec sodales eu, congue vitae sapien. Vivamus imperdiet molestie est quis blanditorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat dui eget quam vestibulum, in mollis elit fermentum. Aenean tristique lectus condimentum mollis aliquet. Maecenas vehicula tincidunt aliquet. Donec mattis ipsum sit amet varius tincidunt. Maecenas varius eros neque, et bibendum tellus euismod nec. Nulla justo neque, vestibulum eu dolor ut, venenatis congue libero. Curabitur ullamcorper, risus eu lacinia lobortis, dolor libero rutrum lorem, id cursus elit nisl sit amet velit. Aliquam ac pharetra felis. Sed in velit sit amet orci volutpat mollis. Fusce euismod tincidunt tortor, ut varius nibh. Sed urna metus, finibus nec sodales eu, congue vitae sapien. Vivamus imperdiet molestie est quis blandit',
         criteria: { novelty: 'it fermentum. Aenean tristique lectus condimentum mollis aliquet. Maecenas vehicula tincidunt aliquet. Donec mattis ipsum sit amet varius tincidunt. Maecenas varius eros neque, et bibendum tellus euismod nec. Nulla justo neque, vestibulum eu dolor ut, venenatis congue libero. Curabitur ullamcorper, risus eu lacinia lobortis, dolor libero rutrum lorem, id cursus elit nisl sit amet velit. Aliq' }
     };
 
@@ -13,13 +13,14 @@ export default function Singlehackathon() {
         <>
             <div className="container">
                 <div className="header"><h3>This is the header placeholder</h3></div>
+                <div className='bg_image'><img src="../public/01.png" alt="bg_img"/></div>
                 <div className='basic'>
-                    <h3>SIA App Challenge</h3>
+                    <h2>SIA App Challenge</h2>
                     <div className='button'><button>Join Hackathon</button></div>
                     <div className='basic_info'>
                         <ul>
-                            <li><span>Prize pool - ${hackathon.prize_pool}</span></li>
-                            <li><span>Start data - {hackathon.start_date} | End data - {hackathon.end_date}</span></li>
+                            <li>🍔<span>Prize pool - ${hackathon.prize_pool}</span></li>
+                            <li>🍟<span>Start data - {hackathon.start_date} | End data - {hackathon.end_date}</span></li>
                         </ul>
                     </div>
                 </div>
