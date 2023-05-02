0c6fa4ea51f74a728c890a1c06a809fbb38bafed
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-03 09:22:58 +1200

    ① Built the front-end Register Hackathon Project Page for Part 2 Participant. ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.

--- src/pages/participantPages/index.jsx
+++ src/pages/participantPages/index.jsx
@@ -1,4 +1,3 @@
 export { default as ParticipantHome } from './participantHome'
-export { default as ParticipantHeader } from './participantHeader'
 export { default as ExploreHackathons } from './explore'
 export { default as MyEvents } from './myEvents'
