9453ed96869566a0c0abbdb085b426ee83ce8cde
xli340 <xli340@aucklanduni.ac.nz>
2023-05-02 17:17:47 +1200

    Change <"> to <'> in the file of explore.jsx and explore.test.jsx

--- __tests__/frontend-tests/explore.test.jsx
+++ __tests__/frontend-tests/explore.test.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { render, getAllByTestId } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Explopre from "../../src/pages/explore";
+import React from 'react';
+import { render, getAllByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Explopre from '../../src/pages/explore';
 
-
-describe("Explopre", () => {
-  it("renders all elements correctly, all elements appeared on the page", () => {
+describe('Explopre', () => {
+  it('renders all elements correctly, all elements appeared on the page', () => {
     const { getAllByRole, getAllByTestId } = render(<Explopre />);
 
     // test buttons
-    const buttonLabels = ["All", "Ongoing", "Finished"];
-    const buttons = getAllByRole("button");
+    const buttonLabels = ['All', 'Ongoing', 'Finished'];
+    const buttons = getAllByRole('button');
     expect(buttons).toHaveLength(3);
     buttons.forEach((button, index) => {
       expect(button).toHaveTextContent(buttonLabels[index]);
     });
 
     // test cards
-    const cards = getAllByTestId("card", { container: document.body });
+    const cards = getAllByTestId('card', { container: document.body });
     expect(cards).toHaveLength(3);
-
   });
 });
