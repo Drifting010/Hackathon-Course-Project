1c9437d84a6c2404dbe599b1ce0a22dfe25fac3e
UoA-EEEEason <703575908@qq.com>
2023-05-03 00:31:37 +1200

    update package-lock.json

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,3 @@
 export { default as Home } from './Home'
-export { default as Header } from './Header'
-export { default as Footer } from './Footer'
+export { default as Header } from '../Components/Header'
+export { default as Footer } from '../Components/Footer'
