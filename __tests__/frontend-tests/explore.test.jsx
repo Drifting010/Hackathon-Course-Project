b979c769bf8f40f20eb61235e61d6f2a23196715
xli340 <xli340@aucklanduni.ac.nz>
2023-05-02 14:53:48 +1200

    add related packages used in explore page add explore frame without function add smoke test of explore.test

---
+++ __tests__/frontend-tests/explore.test.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, getAllByTestId } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Explopre from "../../src/pages/explore";
+
+
+describe("Explopre", () => {
+  it("renders all elements correctly", () => {
+    const { getAllByRole, getAllByTestId } = render(<Explopre />);
+
+    // test buttons
+    const buttonLabels = ["All", "Ongoing", "Finished"];
+    const buttons = getAllByRole("button");
+    expect(buttons).toHaveLength(3);
+    buttons.forEach((button, index) => {
+      expect(button).toHaveTextContent(buttonLabels[index]);
+    });
+
+    // test cards
+    const cards = getAllByTestId("card", { container: document.body });
+    expect(cards).toHaveLength(3);
+
+  });
+});
