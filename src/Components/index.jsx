23c00855e099181f95209d01513b94fb141daa30
Mia190 <117778327+Mia190@users.noreply.github.com>
2023-05-03 20:03:56 +1200

    Frontend - Add homepage and registerpage for part 2 (#18)
    * Built the front-end Header and a simple version of HomePage for Part 2 Participant.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * Merge the code from master branch to ran branch
    
    * Merge the code from master branch to ran branch
    
    * Feature: add test cases
    
    * Update the participantHeader test code.
    
    * Update the participantHeader test code.
    
    * Solve the problem of npm ci failed
    
    * briefly introduce each component by adding comments on the code

---
+++ src/Components/index.jsx
@@ -0,0 +1,1 @@
+export { default as ParticipantHeader } from './participantHeader'
---
+++ src/pages/participantPages/index.jsx
@@ -0,0 +1,4 @@
+export { default as ParticipantHome } from './participantHome'
+export { default as ExploreHackathons } from './explore'
+export { default as MyEvents } from './myEvents'
+export { default as RegisterHackathons } from './register'
