4c4d5572c81e6aad571bd31b56342fadc60f478d
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-06 01:48:23 +1200

    update the account page

--- src/pages/participantPages/account.jsx
+++ src/pages/participantPages/account.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const theme = createTheme();
 
@@ -8,8 +11,32 @@ export default function AccountSetting() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    height: '100vh', // 100% of viewport height
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                }}
+            >
+                <Container maxWidth="md">
+                    {/* Typography component for displaying the hackathon registration title */}
+                    <Typography
+                        component="h4"
+                        variant="h4"
+                        align="center"
+                        color="text.secondary"
+                        gutterBottom
+                        mb={5} // Add margin-bottom here
+                    >
+                        Edit Profile
+                    </Typography>
 
-            
+
+                </Container>
+            </Box>
+
 
         </ThemeProvider>
     );
