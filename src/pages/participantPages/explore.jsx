056a14d0b80bd3a193ee70ba7cf444d956b3eb80
xli340 <xli340@aucklanduni.ac.nz>
2023-05-06 22:17:04 +1200

    Solve the double quote problem

--- src/pages/participantPages/explore.jsx
+++ src/pages/participantPages/explore.jsx
@@ -1,21 +1,16 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState, useEffect } from "react";
-import TemporaryDrawer from "../../Components/TemporaryDrawer";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState, useEffect } from 'react';
+import TemporaryDrawer from '../../Components/TemporaryDrawer';
 import HackathonList from './HackathonList'
 
 const initialFilters = { tag: null, offset: null, status: null}
-const limit = 10
+// const limit = 10
 
 // An array of card objects to be displayed
 const cards = [1, 2, 3];
