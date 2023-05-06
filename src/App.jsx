3bb24850f4ff908bb7c3fc2d0013e1b9c65433a4
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-07 10:17:40 +1200

    Add tagSelector component and simple interests page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// header
 import { HeaderWrapper } from './Components/headerWrapper';
+// footer
 import { HomeFooter } from './Components/';
+//part one - onboarding
+import { Home } from './pages'
+import { Interests } from './pages'
 //part two - signed in participant
 import { ParticipantHome } from './pages/participantPages'
 import { ExploreHackathons } from './pages/participantPages'
 import { MyEvents } from './pages/participantPages'
 import { RegisterHackathons } from './pages/participantPages'
+import { Profile } from './pages/participantPages'
 import { EditProfile } from './pages/participantPages'
 import { AccountSetting } from './pages/participantPages'
 import './App.css'
@@ -27,10 +33,12 @@ export default function App() {
           <main>
             <Routes>
               <Route path="/home" element={<Home />} />
+              <Route path="/interests" element={<Interests />} />
               <Route path="/participant_home" element={<ParticipantHome />} />
               <Route path="/explore_hackathons" element={<ExploreHackathons />} />
               <Route path="/my_events" element={<MyEvents />} />
               <Route path="/register_hackathons" element={<RegisterHackathons />} />
+              <Route path="/profile" element={<Profile />} />
               <Route path="/edit_profile" element={<EditProfile />} />
               <Route path="/account" element={<AccountSetting />} />
             </Routes>
