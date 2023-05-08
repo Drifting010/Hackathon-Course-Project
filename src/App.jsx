d67f1385840edddf6645c0b45c430bf28d41f06f
Mia190 <117778327+Mia190@users.noreply.github.com>
2023-05-08 16:21:20 +1200

    Frontend - Update the import route for onboarding pages (#51)
    * Built the front-end Header and a simple version of HomePage for Part 2 Participant.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * Merge the code from master branch to ran branch
    
    * Merge the code from master branch to ran branch
    
    * Feature: add test cases
    
    * Update the participantHeader test code.
    
    * Update the participantHeader test code.
    
    * Solve the problem of npm ci failed
    
    * briefly introduce each component by adding comments on the code
    
    * Merge from master to ran branch
    
    * Add the account setting page for part 2
    
    * update the account page
    
    * update the account and edit_profile page for Part 2 Participant
    
    * update the account and edit_profile page for Part 2 Participant
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Solve the problem of npm ci failed
    
    * Add tagSelector component and simple interests page.
    
    * Add tagSelector component and simple interests page.
    
    * Add profile page
    
    * Add profile page
    
    * Update the profile page by connecting with the firebase.
    
    * Add theme component
    
    * Add theme component
    
    * Add theme component
    
    * Update signup page
    
    * Update signup and account setting page.
    
    * Update signup and account setting page.
    
    * Update the signup page.
    
    * Update the entire pages
    
    * update the test import route
    
    * Add two pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import { HomeFooter } from './Components/';
 import { Home } from './pages/onboardingPages';
 import Signup from './pages/onboardingPages/signup'
 import { Interests } from './pages/onboardingPages';
+import { RegisterProfileParticipant } from './pages/onboardingPages';
+import { RegisterProfileHost } from './pages/onboardingPages'
 //part two - participant
 import { ParticipantHome } from './pages/participantPages';
 import { ExploreHackathons } from './pages/participantPages';
@@ -44,6 +46,8 @@ export default function App() {
               {/* part 01 */}
               <Route path="/home" element={<Home />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="/register_profile_participant" element={<RegisterProfileParticipant />} />
+              <Route path="/register_profile_host" element={<RegisterProfileHost />} />
               <Route path="/interests" element={<Interests />} />
               {/* part 02 */}
               <Route path="/participant_home" element={<ParticipantHome />} />
