268763f1d0d421d2a20601d38faa461f0e275230
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-09 01:39:54 +1200

    Add test code

---
+++ __tests__/frontend-tests/countrySelect.test.jsx
@@ -0,0 +1,26 @@
+import CountrySelect, { countries } from '../../src/Components/countrySelect';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import sinon from 'sinon';
+
+describe("CountrySelect component", () => {
+    it("renders the Autocomplete component correctly", () => {
+        render(<CountrySelect />);
+        const autocomplete = screen.getByRole('combobox', { name: /choose a country/i });
+        expect(autocomplete).toBeInTheDocument();
+    });
+
+    it("renders all country options when clicked", () => {
+        render(<CountrySelect />);
+        fireEvent.mouseDown(screen.getByRole('combobox', { name: /choose a country/i }));
+
+        countries.forEach((country) => {
+            const option = screen.queryByText(`${country.label} (${country.code})`);
+            expect(option).toBeInTheDocument();
+        });
+    });
+
+    
+
+});
