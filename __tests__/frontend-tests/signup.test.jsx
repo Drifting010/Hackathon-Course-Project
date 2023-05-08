9b79446cb6a1aba7b9ac6bd8b08e295594cded1f
Mia190 <117778327+Mia190@users.noreply.github.com>
2023-05-08 15:53:27 +1200

    Frontend - Add MUI component to signup page & Update the account setting page (#50)
    * Built the front-end Header and a simple version of HomePage for Part 2 Participant.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * Merge the code from master branch to ran branch
    
    * Merge the code from master branch to ran branch
    
    * Feature: add test cases
    
    * Update the participantHeader test code.
    
    * Update the participantHeader test code.
    
    * Solve the problem of npm ci failed
    
    * briefly introduce each component by adding comments on the code
    
    * Merge from master to ran branch
    
    * Add the account setting page for part 2
    
    * update the account page
    
    * update the account and edit_profile page for Part 2 Participant
    
    * update the account and edit_profile page for Part 2 Participant
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Solve the problem of npm ci failed
    
    * Add tagSelector component and simple interests page.
    
    * Add tagSelector component and simple interests page.
    
    * Add profile page
    
    * Add profile page
    
    * Update the profile page by connecting with the firebase.
    
    * Add theme component
    
    * Add theme component
    
    * Add theme component
    
    * Update signup page
    
    * Update signup and account setting page.
    
    * Update signup and account setting page.
    
    * Update the signup page.
    
    * Update the entire pages

--- __tests__/frontend-tests/signup.test.jsx
+++ __tests__/frontend-tests/signup.test.jsx
@@ -1,21 +1,28 @@
 import '@testing-library/jest-dom';
-import { expect, it } from 'vitest';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Signup from '../../src/pages/signup';
 
 describe('Signup', () => {
     it('Renders all input and button elements correctly', () => {
         // render component
-        const { getByPlaceholderText, getByText } = render(<Signup />);
+        const { getByLabelText, getByText, getByRole } = render(<Signup />);
 
-        // test: input and button elements are rendered 
-        expect(getByPlaceholderText('Email (Participant)')).toBeInTheDocument();
-        expect(getByPlaceholderText('Password (Participant)')).toBeInTheDocument();
-        expect(getByPlaceholderText('Confirm Password (Participant)')).toBeInTheDocument();
-        expect(getByPlaceholderText('Email (Host)')).toBeInTheDocument();
-        expect(getByPlaceholderText('Password (Host)')).toBeInTheDocument();
-        expect(getByPlaceholderText('Confirm Password (Host)')).toBeInTheDocument();
+        // test: input and button elements are rendered for participant
+        expect(getByLabelText('Email (Participant)')).toBeInTheDocument();
+        expect(getByLabelText('Password (Participant)')).toBeInTheDocument();
+        expect(getByLabelText('Confirm Password (Participant)')).toBeInTheDocument();
         expect(getByText('Participant')).toBeInTheDocument();
         expect(getByText('Host')).toBeInTheDocument();
+        expect(getByText('Participant Proceed')).toBeInTheDocument();
+
+        // simulate click on "Host" radio button to display host input fields
+        const hostRadioButton = getByRole('radio', { name: 'Host' });
+        fireEvent.click(hostRadioButton);
+
+        // test: input and button elements are rendered for host
+        expect(getByLabelText('Email (Host)')).toBeInTheDocument();
+        expect(getByLabelText('Password (Host)')).toBeInTheDocument();
+        expect(getByLabelText('Confirm Password (Host)')).toBeInTheDocument();
+        expect(getByText('Host Proceed')).toBeInTheDocument();
     });
-})
+});
