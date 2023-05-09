4a5457b315739c1cec10fa341de38e5a0fd77aef
xli340 <xli340@aucklanduni.ac.nz>
2023-05-09 13:36:55 +1200

    resolve conflict against the master

--- src/Components/firebase/firebaseFunction.js
+++ src/Components/firebase/firebaseFunction.js
@@ -256,6 +256,18 @@ const getCurrentUser = () => {
   return null; 
 }
 
+// get all tags
+const getAllTags = async (collectionName) => {
+  try{
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    const documentations = querySnapshot.docs.map(doc => doc.data()['label']);
+    return documentations;
+  }catch (error){
+    console.error('Error getting all tags', error);
+  }
+  
+};
+
 export {
   addHackathon,
   createUserWithEmailAndPasswordFunction,
@@ -273,4 +285,5 @@ export {
   uploadFile,
   downLoadFile,
   setRef,
+  getAllTags,
 };
