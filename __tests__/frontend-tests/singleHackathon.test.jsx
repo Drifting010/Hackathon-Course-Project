3b91ec58c9728adbd164b3fd27e377888e484936
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-11 16:30:44 +1200

    Update the test code

--- __tests__/frontend-tests/singleHackathon.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import SingleHackathon from '../../src/pages/participantPages/singleHackathon';
-
-describe('SingleHackathon', () => {
-    
-  
-});
