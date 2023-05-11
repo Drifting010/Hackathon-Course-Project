d2afbb957c2d6607002e14d53ff0bce9e558f802
xli340 <xli340@aucklanduni.ac.nz>
2023-05-11 19:57:44 +1200

    change the way of getting user role optimistic structure and parameter of component HackathonList

--- src/pages/onboardingPages/homePage.jsx
+++ src/pages/onboardingPages/homePage.jsx
@@ -83,7 +83,7 @@ export default function Album() {
       {/* Adding a container for the card section */}
       {/* display cards */}
       <Box>
-        <HackathonList filters={filters} />
+        <HackathonList filters={filters} pagename={'homePage'}/>
       </Box>
 
     </ThemeProvider>
