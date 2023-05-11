742c0b37f02362b8f89aa213fbef472659e18af9
Ran Ning <rnin190@aucklanduni.ac.nz>
2023-05-11 16:30:40 +1200

    Update the test code

--- __tests__/frontend-tests/singleHackathon.test.jsx
+++ __tests__/frontend-tests/singleHackathon.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { getHackathonAndParticipants } from '../../src/Components/firebase/firebaseFunction';
 import SingleHackathon from '../../src/pages/participantPages/singleHackathon';
 
 describe('SingleHackathon', () => {
