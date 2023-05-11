f87871fcd36ca8196a130192105a7a49ee01ae0e
UoA-EEEEason <126371820+UoA-EEEEason@users.noreply.github.com>
2023-05-11 14:31:24 +1200

    Frontend - update homePage - Yixuan updatestyle branch (#85)
    * hosthome page and dashboard page
    
    * update dashboard - 01
    
    * update dashboard - 02
    
    * update dashboard - 03
    
    * update dashboard - 04
    
    * update comments of all pages
    
    * update dashboard page
    
    * update Icon
    
    * update all pages style
    
    * update pages style 01
    
    * update pages style 02
    
    * update pages style 03
    
    * update pages style 04
    
    * update test
    
    * update homePage
    
    ---------
    
    Co-authored-by: UoA-EEEEason <703575908@qq.com>

--- src/pages/onboardingPages/homePage.jsx
+++ src/pages/onboardingPages/homePage.jsx
@@ -1,24 +1,27 @@
 // Importing necessary dependencies and components from Material UI
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../Components/theme';
+import HackathonList from '../../Components/HackathonList';
+import { useState, useEffect } from 'react';
 
 // An array to hold some dummy card values
-const cards = [1, 2, 3];
+const initialFilters = { tag: null, offset: null, status: null }
+
 
 // Defining the main Album component as the default export
 export default function Album() {
+
+  const [filters, setFilters] = useState(initialFilters);
+  useEffect(() => {
+    setFilters(initialFilters)
+  }, [])
   // Rendering the Album component and returning JSX elements
   return (
     // Wrapping the entire component with the Material UI ThemeProvider component to apply the theme
@@ -78,49 +81,10 @@ export default function Album() {
       </Box>
 
       {/* Adding a container for the card section */}
-      <Container sx={{ py: 8 }} maxWidth="md">
-        {/* Adding a grid layout for the cards */}
-        <Grid container spacing={4}>
-          {/* Mapping through the cards array and creating a card component for each item */}
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card
-                // Setting the card height, display, and flex direction using the Material UI sx prop
-                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                // Setting a data-testid for testing purposes
-                data-testid="card"
-              >
-                {/* Adding an image to the card */}
-                <CardMedia
-                  component="img"
-                  // Adding styles to the image using the Material UI sx prop
-                  sx={{
-                  }}
-                  // Setting the image source to a random image from Unsplash
-                  image="https://source.unsplash.com/random"
-                  alt="random"
-                />
-                {/* Adding card content */}
-                <CardContent sx={{ flexGrow: 1 }}>
-                  {/* Adding a heading to the card */}
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  {/* Adding a description to the card */}
-                  <Typography>
-                    This is a media card. You can use this section to describe the
-                    content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      {/* display cards */}
+      <Box>
+        <HackathonList filters={filters} />
+      </Box>
 
     </ThemeProvider>
   );
