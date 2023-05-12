2d88e6ba2afcefa412b2152c27beda9e6ae45fba
Mia190 <117778327+Mia190@users.noreply.github.com>
2023-05-12 14:43:35 +1200

    Frontend - Update the login and profile pages' style (#128)
    * Built the front-end Header and a simple version of HomePage for Part 2 Participant.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * ① Built the front-end Register Hackathon Project Page for Part 2 Participant.
    ② Built the tset code for both Part 2 Header, Homepage and RegisterPage.
    
    * Merge the code from master branch to ran branch
    
    * Merge the code from master branch to ran branch
    
    * Feature: add test cases
    
    * Update the participantHeader test code.
    
    * Update the participantHeader test code.
    
    * Solve the problem of npm ci failed
    
    * briefly introduce each component by adding comments on the code
    
    * Merge from master to ran branch
    
    * Add the account setting page for part 2
    
    * update the account page
    
    * update the account and edit_profile page for Part 2 Participant
    
    * update the account and edit_profile page for Part 2 Participant
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Add front-end rendering test code for account adn edit_profile pages.
    
    * Solve the problem of npm ci failed
    
    * Add tagSelector component and simple interests page.
    
    * Add tagSelector component and simple interests page.
    
    * Add profile page
    
    * Add profile page
    
    * Update the profile page by connecting with the firebase.
    
    * Add theme component
    
    * Add theme component
    
    * Add theme component
    
    * Update signup page
    
    * Update signup and account setting page.
    
    * Update signup and account setting page.
    
    * Update the signup page.
    
    * Update the entire pages
    
    * update the test import route
    
    * Add test code to submit page.
    
    * Update the profile page
    
    * Update the profile page
    
    * Update the current user
    
    * Update the current user
    
    * Update code
    
    * Update code
    
    * Solve the login problem
    
    * Solve the login problem.
    
    * Update profile
    
    * Update the login page style.
    
    * Update the login page style.
    
    * Update the profile page
    
    * Update the profile page
    
    * Update the login test code

--- src/pages/onboardingPages/Login.jsx
+++ src/pages/onboardingPages/Login.jsx
@@ -1,21 +1,23 @@
 import { Button, InputAdornment, TextField } from "@mui/material";
 import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
 import styles from './Login.module.css'
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import HttpsOutlinedIcon from '@mui/icons-material/HttpsOutlined';
 import { useState } from "react";
 import * as React from 'react';
 import { AppContext } from "../../Components/AppContextProvider";
 import { useNavigate } from "react-router";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 
 function Login() {
     //State variable of Google signin email
 
-    const [loginFail,setLoginFail] = useState("");
+    const [loginFail, setLoginFail] = useState("");
 
     const { getUser, getCurrentUser, signInWithEmailAndPasswordFunction } = React.useContext(AppContext);
 
@@ -29,113 +31,288 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    
+
     const handleLogin = () => {
         setLoginFail("");
-        const login = signInWithEmailAndPasswordFunction(email,password);
-        
-        login.then(function(result) {
-            if(result===null){
+        const login = signInWithEmailAndPasswordFunction(email, password);
+
+        login.then(function (result) {
+            if (result === null) {
                 setLoginFail("Invalid username and password");
-            }else{
+            } else {
                 const user = getCurrentUser();
                 const userDetails = getUser(user.email);
 
-                userDetails.then(function(result){
-                    if(result.role==="participant"){
+                userDetails.then(function (result) {
+                    if (result.role === "participant") {
                         navigate("/explore_hackathons");
-                    }else if(result.role==="host"){
+                    } else if (result.role === "host") {
                         navigate("/dashboard");
                     }
                 });
             }
-        },function(){
+        }, function () {
             setLoginFail("An error occured.")
         });
     }
 
     return (
-        <div>
-            <h1>Log in to your account</h1>
-            <p>Welcome back</p>
-            <div>
-                <Button
-                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
-                    variant="contained"
-                    startIcon={<GoogleIcon/>}
-                    onClick={handleGoogleLogin}
+        <>
+            {/* Outer Box for centering the inner content */}
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                }}
+            >
+                {/* Inner Box containing the form */}
+                <Box
+                    sx={{
+                        textAlign: 'left',
+                    }}
                 >
-                    Google
-                </Button>
-                <Button
-                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
-                    variant="contained"
-                    startIcon={<GitHubIcon/>}
-                >
-                    GitHub
-                </Button>
-                <Button
-                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
-                    variant="contained"
-                    startIcon={<LinkedInIcon/>}
-                >
-                    Linkedin
-                </Button>
-                <Button
-                    sx={{backgroundColor: 'darkgray', ':hover': {backgroundColor: 'dimgray'},marginLeft: '10px',marginRight: '10px'}}
-                    variant="contained"
-                    startIcon={<FacebookIcon/>}
-                >
-                    Facebook
-                </Button>
-            </div>
-            <p>or continue with email</p>
-            <div className={styles.container}>
-                <TextField
-                    required
-                    name="email-required"
-                    id="email-required"
-                    label="Enter email"
-                    value={email}
-                    onChange={(e)=> {setEmail(e.target.value)}}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                            <EmailIcon />
-                            </InputAdornment>
-                        ),
+                    {/* Heading */}
+                    <Typography
+                        sx={{
+                            mb: '10px',
+                            fontFamily: 'Inter',
+                            fontStyle: 'normal',
+                            fontSize: '25px',
+                            fontWeight: 700,
+                            letterSpacing: '0.75px',
+                            color: '#FFFFFF',
                         }}
-                />
-            </div>
-            <div>
-                <TextField
-                    id="password-required"
-                    label="Password"
-                    type="password"
-                    required
-                    value={password}
-                    onChange={(e)=> {setPassword(e.target.value)}}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                            <LockIcon />
-                            </InputAdornment>
-                        ),
+                    >
+                        Log in to your account
+                    </Typography>
+
+                    {/* Subheading */}
+                    <Typography
+                        sx={{
+                            mb: '30px',
+                            fontFamily: 'Inter',
+                            fontStyle: 'normal',
+                            fontSize: '14px',
+                            fontWeight: 300,
+                            letterSpacing: '0.75px',
+                            color: '#FFFFFF',
                         }}
-                />
-            </div>
-            <a href="">Forgot password?</a>
-            <p style={{color:'red'}}>{loginFail}</p>
-            <div>
-                <Button
-                    sx={{backgroundColor: 'orange', ':hover': {backgroundColor: 'sandybrown'},width: '250px'}}
-                    variant="contained"
-                    onClick={handleLogin}
-                >
-                    Log in
-                </Button>
-            </div>
-        </div>
+                    >
+                        Welcome back
+                    </Typography>
+
+                    <div>
+                        <Button
+                            sx={{
+                                background: '#21262D',
+                                ':hover': { backgroundColor: 'dimgray' },
+                                arginLeft: '10px',
+                                marginRight: '10px',
+                                border: '1px solid #30363D',
+                                borderRadius: '15px',
+                                width: '132px',
+                                height: '57px',
+                                color: '#D9D9D9',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '12px',
+                                fontWeight: 500,
+                                textTransform: 'none',
+                            }}
+                            variant="contained"
+                            startIcon={<GoogleIcon />}
+                            onClick={handleGoogleLogin}
+                        >
+                            Google
+                        </Button>
+                        <Button
+                            sx={{
+                                background: '#21262D',
+                                ':hover': { backgroundColor: 'dimgray' },
+                                arginLeft: '10px',
+                                marginRight: '10px',
+                                border: '1px solid #30363D',
+                                borderRadius: '15px',
+                                width: '132px',
+                                height: '57px',
+                                color: '#D9D9D9',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '12px',
+                                fontWeight: 500,
+                                textTransform: 'none',
+                            }}
+                            variant="contained"
+                            startIcon={<GitHubIcon />}
+                        >
+                            GitHub
+                        </Button>
+                        <Button
+                            sx={{
+                                background: '#21262D',
+                                ':hover': { backgroundColor: 'dimgray' },
+                                arginLeft: '10px',
+                                marginRight: '10px',
+                                border: '1px solid #30363D',
+                                borderRadius: '15px',
+                                width: '132px',
+                                height: '57px',
+                                color: '#D9D9D9',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '12px',
+                                fontWeight: 500,
+                                textTransform: 'none',
+                            }}
+                            variant="contained"
+                            startIcon={<LinkedInIcon />}
+                        >
+                            Linkedin
+                        </Button>
+                        <Button
+                            sx={{
+                                background: '#21262D',
+                                ':hover': { backgroundColor: 'dimgray' },
+                                arginLeft: '10px',
+                                marginRight: '10px',
+                                border: '1px solid #30363D',
+                                borderRadius: '15px',
+                                width: '132px',
+                                height: '57px',
+                                color: '#D9D9D9',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '12px',
+                                fontWeight: 500,
+                                textTransform: 'none',
+                            }}
+                            variant="contained"
+                            startIcon={<FacebookIcon />}
+                        >
+                            Facebook
+                        </Button>
+                    </div>
+
+                    <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ my: 2 }}
+                    >
+                        <Box
+                            flexGrow={1}
+                            borderBottom="1px solid #21262D"
+                            mr={2}
+                        />
+                        <Typography
+
+                            sx={{
+                                color: '#F3F3F3',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '10px',
+                                fontWeight: 500,
+                            }}
+                        >
+                            or continue with email
+                        </Typography>
+                        <Box
+                            flexGrow={1}
+                            borderBottom="1px solid #21262D"
+                            ml={2}
+                        />
+                    </Box>
+
+                    <div className={styles.container} >
+                        <TextField
+                            required
+                            sx={{ background: '#21262D', width: "425px", }}
+                            name="email-required"
+                            id="email-required"
+                            label="Enter email"
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <EmailIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </div>
+
+                    <div>
+                        <TextField
+                            id="password-required"
+                            sx={{ background: '#21262D', width: "425px", mb: 2, }}
+                            label="Enter Password"
+                            type="password"
+                            required
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <HttpsOutlinedIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </div>
+
+                    <div>
+                        <Typography
+                            href=""
+                            sx={{
+                                mb: '30px',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontSize: '12px',
+                                fontWeight: 500,
+                                letterSpacing: '0.75px',
+                                color: '#326AD5',
+                            }}
+                        >
+                            Forgot password?
+                        </Typography>
+                    </div>
+
+                    <Typography
+                        style={{
+                            fontFamily: 'Inter',
+                            fontStyle: 'normal',
+                            fontSize: '12px',
+                            fontWeight: 500,
+                            color: '#FF6262',
+                        }}>
+                        {loginFail}
+                    </Typography>
+
+                    <div>
+                        <Button
+                            sx={{
+                                width: '425px',
+                                height: '40px',
+                                background: '#FF9300',
+                                textTransform: 'none',
+                                borderRadius: '5px',
+                                fontFamily: 'Inter',
+                                fontStyle: 'normal',
+                                fontWeight: 700,
+                                fontSize: '14px',
+                                color: '#F7F7FC',
+                            }}
+                            onClick={handleLogin}
+                        >
+                            Log in
+                        </Button>
+                    </div>
+                </Box>
+            </Box>
+        </>
     )
 }
 
